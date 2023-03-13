fb2343696da9fc91ce66f7982e5bc3097af6e210
Akshay4955 <akshayshedge11@gmail.com>
2023-03-13 19:44:10 +0530

    Added: Code to add multiple nodes in binary tree

---
+++ src/main/java/com/bridgelabz/hashmap/MyBinaryTreeWithMultipleNodesTest.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.hashmap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyBinaryTreeWithMultipleNodesTest {
+    @Test
+    public void GivenNumbersWhenAddedToBinaryTreeShouldReturnSize() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(65);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        int size = myBinaryTree.getSize();
+        Assert.assertEquals(13, size);
+    }
+}
